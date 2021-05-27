afe101a271f16d7cc24396fbd13ca666949de62f
Cédric B <74956171+b-ced@users.noreply.github.com>
2021-05-27 23:09:51 +0200

    cssminify.php
    Function to minify CSS code

---
+++ cssminify.php
@@ -0,0 +1,17 @@
+<?php
+
+// Your CSS file
+$css=file_get_contents('style.css');
+
+// All the magic happens
+$css=preg_replace(
+    ["/\r|\n|\t/",
+    "/\s{2,}/", "/\s*(;|\{|\}|\:|\,)\s*(\w*)/", '/\/\*.*\*\//U'],
+    ['', '', '\1\2', ''],
+    $css
+);
+
+// output de CSS code minified, or you may want to put it back in a CSS file
+echo $css;
+
+?>
